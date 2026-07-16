0323707463640b1430d6bb034836bf370627e13b
Emran Uddin <bemranuddin2006@gmail.com>
2026-07-16 11:08:15 +0600

    use class method

--- b.py
+++ b.py
@@ -3,3 +3,23 @@ b = 10
 
 def add(a,b):
     return a + b
+
+class calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+    def multiply(self):
+        return self.a * self.b
+
+    def divide(self):
+        if self.b != 0:
+            return self.a / self.b
+        else:
+            return "Cannot divide by zero"
